7a60c0b733dff9da8c4de3b3aa37d679d62b4db0
equation314 <equation618@gmail.com>
2025-01-27 02:32:42 +0000

    Deploying to gh-pages from @ arceos-org/axhal_crates@5f3b3833972be92452a0ffc09042041c9f892bd4 🚀

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["axhal_cpu","axhal_plat","axhal_plat_macros"];
+//{"start":21,"fragment_lengths":[11,13,20]}
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axhal_cpu",["",[["x86_64",[],["context.rs","gdt.rs","idt.rs","mod.rs"]]],["lib.rs","trap.rs"]]],["axhal_plat",["",[],["console.rs","init.rs","irq.rs","lib.rs","mem.rs","power.rs","time.rs"]]],["axhal_plat_macros",["",[],["lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[97,96,41]}
